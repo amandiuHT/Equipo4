9fdf8fafee8b82cdfb95d5bc772c6d8979f9f4c9
DanielaSanchez130571 <daniela130571@gmail.com>
2023-07-11 20:44:08 -0600

    Formulario primer input y Js Requisito Nombre 4 letras

---
+++ js/validacion.js
@@ -0,0 +1,18 @@
+// Para valiad el formulario se van a utilizar los eventos de js
+
+// 1.- Asignar los campos 
+const nameField = document.getElementById("Nombre");
+const apellidoField = document.getElementById("Apellido");
+const mailField = document.getElementById("email");
+
+//2.- Asignar los eventos 
+nameField.addEventListener("blur", function (e) {
+    const fieldValue = e.target.value;
+    if (fieldValue.length < 3) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'El nombre debe tener al menos 4 letras!',
+        })
+    }
+})
